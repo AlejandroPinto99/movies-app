6800a88e141af9d90929573c4d0688c3d6598a3d
AlejandroPinto99 <a.pinto8899@gmail.com>
2025-04-04 16:00:21 -0600

    Setting fetch movies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({ highlight, icon, title, focused }: any) => {
         return (
         <ImageBackground
             source={highlight}
-            className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 justify-center items-center rounded-full overflow-hidden'
+            className='flex flex-row w-full flex-1 min-w-[112px] min-h-20 justify-center items-center rounded-full overflow-hidden pt-2'
         >
             <Image source={icon}
                 tintColor="#151312"
@@ -20,7 +20,7 @@ const TabIcon = ({ highlight, icon, title, focused }: any) => {
         )
     } else {
         return (
-            <View className="size-full justify-center items-center mt-4 rounded-full">
+            <View className="size-full justify-center items-center mt-2 rounded-full">
                 <Image
                     source={icon}
                     tintColor="#A8B5DB"
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,33 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View
-      className="flex-1 justify-center items-center"
+      className="flex-1 bg-primary"
     >
-      <Text className="text-5xl text-blue-500">Welcome</Text>
+      <Image source={images.bg} className="absolute z-0" />
+      <ScrollView className="flex-1 px-5 "
+      showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          padding: 10
+        }}
+      >
+        <Image source={icons.logo } className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
